25e7d5d08c21c3e3110131510d951a8850231618
Kaustubh Daftuar <kaustubhdaftuar@gmail.com>
2026-02-10 11:52:18 +0530

    fix: move terraform backend to eu-north-1 s3 bucket

--- deploy/fragile/backend.tf
+++ deploy/fragile/backend.tf
@@ -2,10 +2,9 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket         = "your-tf-state-bucket"
-    key            = "fragile/terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket  = "self-healing-tf-state-eu"
+    key     = "fragile/terraform.tfstate"
+    region  = "eu-north-1"
+    encrypt = true
   }
 }
