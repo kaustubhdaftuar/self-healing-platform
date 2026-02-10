df39873bfcd779d0be37794bbba072c979c3becb
Kaustubh Daftuar <kaustubhdaftuar@gmail.com>
2026-02-10 15:20:35 +0530

    new commit

--- deploy/fragile/healer.tf
+++ deploy/fragile/healer.tf
@@ -3,13 +3,9 @@ resource "aws_lambda_function" "healer" {
   runtime       = "python3.10"
   handler       = "app.lambda_handler"
 
-  filename = var.use_artifacts
-  ? "${path.module}/../../src/healer/healer.zip"
-  : null
+  filename = var.use_artifacts ? "${path.module}/../../src/healer/healer.zip" : null
 
-  source_code_hash = var.use_artifacts
-  ? filebase64sha256("${path.module}/../../src/healer/healer.zip")
-  : null
+  source_code_hash = var.use_artifacts ? filebase64sha256("${path.module}/../../src/healer/healer.zip") : null
 
   role = aws_iam_role.healer_role.arn
 
